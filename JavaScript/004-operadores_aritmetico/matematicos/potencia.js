4a4e33add90beeccf40587e42b37c455c38d9928
Rafael_Pereira_Duque <rafaellduque049@gmail.com>
2023-08-13 13:23:43 -0300

    Operadores aritméticos.
    Conhecendo mais operadores aritméticos matemáticos como: potência e reminder(divisão com resto).

---
+++ JavaScript/004-operadores_aritmetico/matematicos/potencia.js
@@ -0,0 +1,3 @@
+
+// Para fazermos uma conta de potência, utilizamos dois asterisco. Como exemplificado abaixo:
+console.log(10 ** 2) // 10 ** 2 é igual à 10^2 ou 10², como desmontrada na matemática.
---
+++ JavaScript/004-operadores_aritmetico/matematicos/reminder.js
@@ -0,0 +1,11 @@
+// Reminder, é um operador aritmético da programação que faz o trabalho de fazer uma divisão com resto.
+
+console.log(6 % 2) // A saída é zero, já que 6 dividido por 2, não resta nenhum valor.
+
+console.log(5 % 2) // Já 5 dividido por 2, resta 1, já que ele não pode ser dívidido por dois, sem ser por número flutuante.
+
+
+
+// Com isso, é possível verificar se um valor é ímpar ou par, algo muito importante em alguns momentos.
+console.log( 10 % 2 == 0 ) // Verificar se um valor é ìmpar
+console.log( 10 % 2 != 0 ) // Verificar se um valor é par
