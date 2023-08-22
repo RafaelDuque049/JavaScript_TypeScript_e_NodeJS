1e44c2ab62dd2ae7e218ef22afa613980d72c9c7
Rafael_Pereira_Duque <rafaellduque049@gmail.com>
2023-08-21 22:28:22 -0300

    Condicionais (IF, ELSE, ELSE IF).
    Entendendo e utilizando Condicionais no JavaScript.

---
+++ JavaScript/006-condicionais/condiçoes-if-else.js
@@ -0,0 +1,18 @@
+let num = 10;
+
+if(num >= 15){
+    console.log('O número é maior ou igual à 15')
+} else {
+    console.log('O valor é menor que 15.')
+}
+
+
+let bool = true
+
+if (bool == true){
+    console.log('O valor atual é Verdadeiro(true)')
+} else if (bool == false) {
+    console.log('O valor atual é False(false)')
+} else{
+    console.log('O valor atual é diferente de Verdadeiro e False.')
+}
