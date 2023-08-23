6fa80100cd84d1b6014cc8ee1eec47a726815467
Rafael_Pereira_Duque <rafaellduque049@gmail.com>
2023-08-22 21:46:47 -0300

    Update index.js
    Sintax e comentário: Documentação de função.

--- JavaScript/002-Sintaxe e comentario/index.js
+++ JavaScript/002-Sintaxe e comentario/index.js
@@ -8,3 +8,19 @@ console.log('Opa, boa tarde!')
 2.comentário
 3.comentário
 */
+
+
+// Logo abaixo, está um exemplo de comentário para documentação de funções(JSDoc).
+
+/** Retorna o valor definido em "numero" pelo número adicionado em "multiplicador". 
+ * @param {number} numero - Valor a ser multiplicado.
+ * @param {number} multiplicador - Número para multiplicar o valor desejado.
+ * @returns {number} Retorna o número multiplicado pelo valor definido.
+ */
+function Multiplicar(numero, multiplicador){
+    return numero * multiplicador
+}
+
+const result = Multiplicar(10, 3);
+
+console.log(result)
